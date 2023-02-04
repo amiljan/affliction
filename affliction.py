f3b9321479125ff662a5c53abdda06db12cfc3a7
neovonpoet <andro.miljan@gmail.com>
2023-02-04 11:48:21 +0100

    gitpython

--- affliction.py
+++ affliction.py
@@ -1,4 +1,6 @@
 import json
+from git import Repo
+#pip install gitpython
 
 class Character():
     def __init__(self,name):
@@ -33,6 +35,22 @@ def generate_json(characters):
             "penance" : i.penance
         }
 
+def git_push():  
+
+    PATH_OF_GIT_REPO = r'path\to\your\project\folder\.git'  # make sure .git folder is properly configured
+    COMMIT_MESSAGE = 'comment from python script'
+
+    def git_push():
+        try:
+            repo = Repo(PATH_OF_GIT_REPO)
+            repo.git.add(update=True)
+            repo.index.commit(COMMIT_MESSAGE)
+            origin = repo.remote(name='origin')
+            origin.push()
+        except:
+            print('Some error occured while pushing the code')    
+
+    git_push()
 
     with open("party.json","w") as party_json:
         party_json.write(json.dumps(party))
